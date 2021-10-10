ed88d35b7d63cd0f335d1839c4f895d3e36f9370
Aristela Marku <aristela.marku@gmail.com>
2021-10-10 13:22:02 +0200

    commit tree 789af16a65976c67fdbdcc1ac4262ef079c4f190

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,8 +1,19 @@
 import React from "react";
 import whoseHouse from "./houses/whoseHouse";
+import { colors, gryffMascot } from "./houses/Gryffindor"
+import Hufflepuff from "/house/Hufflepuff"
 
 function Hogwarts() {
   whoseHouse(); // => "HAGRID'S HOUSE!"
-
-  return <h1>Welcome to Hogwarts!</h1>;
+  return (
+  <div>
+  <h1>Welcome to Hogwarts!</h1>;
+  <Hufflepuff />
+  </div>
+  )
 }
+
+console.log(colors);
+// > 'Scarlet and Gold'
+
+gryffMascot();
